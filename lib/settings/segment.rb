3636eda6f2c7bee168513316aa7d3facc4acc840
Daniel Olivares <danieloddl2@hotmail.com>
2024-03-12 10:33:28 -0600

    fix(segment): Allow segment write key to not be present in credentials

--- lib/settings/segment.rb
+++ lib/settings/segment.rb
@@ -6,7 +6,7 @@ initializer 'segment.rb', <<~RB
   # frozen_string_literal: true
 
   options = {
-    write_key: Rails.application.credentials.fetch(:segment, {})[:write_key],
+    write_key: Rails.application.credentials.fetch(:segment, {})[:write_key] || '',
     on_error: proc { |_status, msg| Sentry.capture_message(msg) },
     stub: Rails.env.local?
   }
